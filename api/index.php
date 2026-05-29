0c0f4db64d0a42dbdf2e888fe0faa5c00d41ae07
Yesha Atmaja <yesha.atmaja@gmail.com>
2026-05-29 22:23:48 +0700

    Hosting API lagi

--- api/index.php
+++ api/index.php
@@ -2,4 +2,5 @@
 // api/index.php
 //require __DIR__ . '/../vendor/autoload.php';
 //require __DIR__ . '/../public/index.php';
-require __DIR__ . '/../index.php';
+
+require __DIR__ . '/../public/index.php';
