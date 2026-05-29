78fbf2540871bd59053ed3f6a6fccdfac79eeea8
Yesha Atmaja <yesha.atmaja@gmail.com>
2026-05-29 22:52:42 +0700

    Hosting API Vercel

--- public/index.php
+++ public/index.php
@@ -24,7 +24,22 @@ $app->get('/simulasi-nested', function (Request $request, Response $response) {
         "id_resep" => 101,
         "nama_pasien" => "Budi Santoso",
         "dokter" => "dr. Andi",
-        "tanggal" => "2026-03-04"
+        "tanggal" => "2026-03-04",
+        "status" => "diproses",
+        "detail_obat" => [
+            [
+                "id_obat" => 12,
+                "nama_obat" => "Amoxicillin",
+                "jumlah" => 10,
+                "dosis" => "3x1 sesudah makan"
+            ],
+            [
+                "id_obat" => 45,
+                "nama_obat" => "Paracetamol",
+                "jumlah" => 5,
+                "dosis" => "1x1 jika demam"
+            ]
+        ]
     ];
 
     $response->getBody()->write(
