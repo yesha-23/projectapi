f68287bcbc3a81ba5353fca92fc942ef81d21b05
Yesha Atmaja <yesha.atmaja@gmail.com>
2026-05-29 22:47:00 +0700

    fix slim routes vercel

--- public/index.php
+++ public/index.php
@@ -8,35 +8,31 @@ use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
+// IMPORTANT
+$app->setBasePath('');
+
+$app->get('/', function (Request $request, Response $response) {
+
+    $response->getBody()->write("API berjalan");
+
+    return $response;
+});
+
 $app->get('/simulasi-nested', function (Request $request, Response $response) {
 
     $data = [
         "id_resep" => 101,
         "nama_pasien" => "Budi Santoso",
         "dokter" => "dr. Andi",
-        "tanggal" => "2026-03-04",
-        "status" => "diproses",
-        "detail_obat" => [
-            [
-                "id_obat" => 12,
-                "nama_obat" => "Amoxicillin",
-                "jumlah" => 10,
-                "dosis" => "3x1 sesudah makan"
-            ],
-            [
-                "id_obat" => 45,
-                "nama_obat" => "Paracetamol",
-                "jumlah" => 5,
-                "dosis" => "1x1 jika demam"
-            ]
-        ]
+        "tanggal" => "2026-03-04"
     ];
 
     $response->getBody()->write(
         json_encode($data, JSON_PRETTY_PRINT)
     );
 
-    return $response->withHeader('Content-Type', 'application/json');
+    return $response
+        ->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
